54fa75bc4535fece9d0bf6d1d980feb0a9ed097b
smcmobdev <himanshu8864@smcindiaonline.com>
2026-03-18 08:22:01 +0530

    chore: update android minSdkVersion to 24 and replace deprecated APIs

--- android/src/main/java/com/xamdesign/safe_device/ExternalStorage/ExternalStorageCheck.java
+++ android/src/main/java/com/xamdesign/safe_device/ExternalStorage/ExternalStorageCheck.java
@@ -13,30 +13,14 @@ public class ExternalStorageCheck {
      * @return <code>true</code> if the application is installed on the sd card
      */
     public static boolean isOnExternalStorage(Context context) {
-        // check for API level 8 and higher
-        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.ECLAIR_MR1) {
-            PackageManager pm = context.getPackageManager();
-            try {
-                PackageInfo pi = pm.getPackageInfo(context.getPackageName(), 0);
-                ApplicationInfo ai = pi.applicationInfo;
-                return (ai.flags & ApplicationInfo.FLAG_EXTERNAL_STORAGE) == ApplicationInfo.FLAG_EXTERNAL_STORAGE;
-            } catch (PackageManager.NameNotFoundException e) {
-                // ignore
-            }
-        }
-
-        // check for API level 7 - check files dir
+        PackageManager pm = context.getPackageManager();
         try {
-            String filesDir = context.getFilesDir().getAbsolutePath();
-            if (filesDir.startsWith("/data/")) {
-                return false;
-            } else if (filesDir.contains("/mnt/") || filesDir.contains("/sdcard/")) {
-                return true;
-            }
-        } catch (Throwable e) {
+            PackageInfo pi = pm.getPackageInfo(context.getPackageName(), 0);
+            ApplicationInfo ai = pi.applicationInfo;
+            return (ai.flags & ApplicationInfo.FLAG_EXTERNAL_STORAGE) == ApplicationInfo.FLAG_EXTERNAL_STORAGE;
+        } catch (PackageManager.NameNotFoundException e) {
             // ignore
         }
-
         return false;
     }
 
